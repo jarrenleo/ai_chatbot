d08ca83355b7e0114a94f02201dec61ebb9f3fac
jarrenleo <jarren.leoo@gmail.com>
2024-06-21 18:14:51 +0800

    Update model

--- modules/anthropic.js
+++ modules/anthropic.js
@@ -21,7 +21,7 @@ export default class AnthropicAPI {
         messages = [{ role: "user", content: currentPrompt }];
 
       const completion = await this.anthropic.messages.create({
-        model: "claude-3-opus-20240229",
+        model: "claude-3-5-sonnet-20240620",
         max_tokens: 4096,
         messages,
       });
